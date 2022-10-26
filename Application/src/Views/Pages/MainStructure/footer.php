2614075b70ac2572c2ae225f03443b383043d590
MoisesK <moises979@gmail.com>
2022-10-26 20:54:12 -0300

    feat: Init project, Views base and Structure.

---
+++ Application/src/Views/Pages/MainStructure/footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center mb-3">
+    <hr>
+    <a href="https://github.com/MoisesK/" class=""><i class="bi bi-github"></i></a>
+    Moisés Dev - 2022
+</footer>
---
+++ Application/src/Views/Pages/MainStructure/page.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{title}}</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body class="bg-dark text-light">
+    <div class="container">
+        {{header}}
+
+        {{content}}
+
+        {{footer}}
+    </div>
+</body>
+
+</html>
